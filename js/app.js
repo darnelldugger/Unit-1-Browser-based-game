509f69f2001d3de36f7b8f4dd2eca8550c643b37
darnelld <darnelldugger@gmail.com>
2022-02-15 13:38:40 -0500

    finish objects array

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
 /*-------------------------------- Constants --------------------------------*/
-const qbList = {
-  name: 'Brett Favre',
-  image: 'images/brett-favre.jpg',
-}
-console.log(qbList.image)
+const qbList = [
+{ name: "Brett Favre", image: "brett-favre.jpg"},
+{ name: "Cam Newton",photo_url: "images/cam-newton.jpg" },
+
+]
+console.log(qbList[0].photo_url)
 /*-------------------------------- Variables --------------------------------*/
 let winner, timeOut
 
 
 /*------------------------ Cached Element References ------------------------*/
-const img = document.querySelector('img')
+const img = document.getElementById('image-area')
 const scoreCount = document.getElementById('score-board')
 const countDown = document.getElementById('time-clock')
-const skipBtn = document.getElementById('next')
+const nextBtn = document.getElementById('next')
 const resetBtn = document.getElementById('startOver')
 const messages = document.getElementById('msg')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+nextBtn.addEventListener('click', (evt) =>{
+  evt.preventDefault()
+  render()
+})
 
 /*-------------------------------- Functions --------------------------------*/
 init ()
 
 function init () {
 
+}
+
+function render() {
+  img.innerHTML = qbList[0].image
 }
 
 
