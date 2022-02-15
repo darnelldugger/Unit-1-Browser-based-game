12ac8778b63281ac92313249147794b529300005
darnelld <darnelldugger@gmail.com>
2022-02-15 10:15:41 -0500

    add cached element

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ let winner, timeOut
 
 /*------------------------ Cached Element References ------------------------*/
 const imageUpload = document.querySelector('#image-area')
-
+const scoreCount = document.querySelector('#score-board')
+const countDown = document.querySelector('#time-clock')
+const skipBtn = document.querySelector('#next')
+const resetBtn = document.querySelector('#startOver')
+const messages = document.querySelector('#msg')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
