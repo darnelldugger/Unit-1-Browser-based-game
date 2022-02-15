9fc9cf76d60b4639161af32d5c63f77c84174c3d
darnelld <darnelldugger@gmail.com>
2022-02-15 18:32:32 -0500

    generate random index

--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 /*-------------------------------- Constants --------------------------------*/
-const qbList = {
-  image: "../images/brett-favre.jpg",
-  choices: ["Tom Brady", "Jim Kelly", "Greg Flaukler", "Brett Farve"],
-  correctAnswer: "Brett Favre"
-}
-console.log(qbList)
+const qbList =[
+  {
+    image: "../images/brett-favre.jpg",
+    choices: ["Tom Brady", "Jim Kelly", "Greg Flaukler", "Brett Favre"],
+    correctAnswer: 3,
+    asked: false,
+  },
+  {
+    image: "../images/cam-newton.jpg",
+    choices: ["Micheal Vick", "Cam Newton", "Dante Culpepper", "Carson Wentze"],
+    correctAnswer: 1,
+    asked: false,
+  },
+  {
+    image: "../images/joe-montana.jpg",
+    choices: ["Joe Montana", "Cam Newton", "Dante Culpepper", "Carson Wentze"],
+    correctAnswer: 0,
+    asked: false,
+  },
+  {
+    image: "../images/donovan-mcnabb.jpg",
+    choices: ["Micheal Vick", "Cam Newton", "Donovan McNabb", "Carson Wentze"],
+    correctAnswer: 2,
+    asked: false,
+  },
+
+]
+
+
+
 /*-------------------------------- Variables --------------------------------*/
-let winner, timeOut
+let winner, timeOut, questions
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -16,6 +40,7 @@ const nextBtn = document.getElementById('next')
 const countDown = document.getElementById('time-clock')
 const resetBtn = document.getElementById('startOver')
 const messages = document.getElementById('msg')
+const choices =document.querySelectorAll('#btn')
 
 /*----------------------------- Event Listeners -----------------------------*/
 nextBtn.addEventListener('click', (evt) =>{
@@ -24,17 +49,31 @@ nextBtn.addEventListener('click', (evt) =>{
 })
 
 /*-------------------------------- Functions --------------------------------*/
-
+//when ready to add start button, remove this line
+// start button can call upon init()
 init ()
 
 function init() {
+  questions = [...qbList]
+  const idx = getRandomIndex()
+  console.log(questions[idx])
 }
-render()
+// render()
 
 function render(){
   // imageQuest.src = qbList.image
   const img =document.createElement("img")
   img.src = qbList.image 
   imageQuest.appendChild(img)
+}
+
+function getRandomIndex(){
+  const idx = Math.floor(Math.random() * questions.length)
+  if (questions[idx].asked === true) {
+    getRandomIndex()
+  } else {
+    return idx
+  }
+
 }
 
